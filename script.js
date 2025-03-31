ba2309ce57b1e79c46b6db48f3629dde9f96b864
Akshay-2396 <itsakshay.ak96@gmail.com>
2025-03-31 14:51:18 +0530

    dom

---
+++ script.js
@@ -0,0 +1,64 @@
+
+// const heading = document.createElement("h1")
+// heading.innerHTML = "<b> Hi All I'm Akshay </b>"
+// heading.setAttribute=("class","head")
+// document.body.append(heading)
+
+
+
+// const heading = document.createElement("h1")
+// const heading2 = document.createElement("h1")
+// const header = document.createElement("header")
+// const navigation = document.createElement("nav")
+// const order = document.createElement("ul")
+// const list1 = document.createElement("li")
+// const list2 = document.createElement("li")
+// const list3 = document.createElement("li")
+// const main = document.createElement("main")
+// const section = document.createElement("section")
+
+// heading.innerHTML = "<i>Create An Element Session Task</i>"
+// heading2.innerHTML = "<i> Above one created Dynamically </i>"
+// list1.innerText = "About"
+// list2.innerText= "Contact"
+// list3.innerText = "Pricing"
+
+// heading.classList.add("text-center", "text-3xl", "text-black-800", "head")
+// heading2.classList.add("text-center", "text-3xl", "text-red-800", "head")
+// header.setAttribute("class", "bg-purple-500");
+// order.classList.add("flex", "justify-center", "space-evenly", "gap-8")
+// list1.classList.add("list-none", "text-black", "cursor-pointer")
+// list2.classList.add("list-none", "text-black", "cursor-pointer")
+// list3.classList.add("list-none", "text-black", "cursor-pointer")
+
+// order.append(list1,list2,list3)
+// navigation.appendChild(order)
+// header.appendChild(navigation)
+// section.appendChild(heading2)
+// main.appendChild(section)
+// document.body.append(heading, header, main)
+
+
+function dynamic(tagname,content,attributename,attributevalue){
+    const ele = document.createElement(tagname);
+    ele.innerHTML =content;
+    ele.setAttribute(attributename,attributevalue)
+    return ele;
+}
+const heading = dynamic("h1","create Element Task","class","head")
+const heading1 = dynamic("h1","above muliple","class","head")
+const header = dynamic("header","","class","header")
+const navigation = dynamic("nav","","class","nav")
+const order = dynamic("ul","","style","display:flex; justify-content:space-between; align-items:center; gap:20px; font-size:2rem")
+const list = dynamic("li","about","style","list-style:none")
+const list1 = dynamic("li","pricing","style","list-style:none")
+const list2 = dynamic("li","contact","style","list-style:none")
+const main = dynamic("main","","class","container")
+const section = dynamic("section","","class","container")
+
+order.append(list,list1,list2)
+navigation.appendChild(order)
+header.appendChild(navigation)
+section.appendChild(heading1)
+main.appendChild(section)
+document.body.append(heading,header,main)
