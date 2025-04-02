8dc9d40c3afe69603a46c1721649006f516f2b0b
Akshay-2396 <itsakshay.ak96@gmail.com>
2025-04-03 00:52:13 +0530

    done

--- script.js
+++ script.js
@@ -39,26 +39,31 @@
 // document.body.append(heading, header, main)
 
 
-function dynamic(tagname,content,attributename,attributevalue){
-    const ele = document.createElement(tagname);
-    ele.innerHTML =content;
-    ele.setAttribute(attributename,attributevalue)
-    return ele;
-}
-const heading = dynamic("h1","create Element Task","class","head")
-const heading1 = dynamic("h1","above muliple","class","head")
-const header = dynamic("header","","class","header")
-const navigation = dynamic("nav","","class","nav")
-const order = dynamic("ul","","style","display:flex; justify-content:space-between; align-items:center; gap:20px; font-size:2rem")
-const list = dynamic("li","about","style","list-style:none")
-const list1 = dynamic("li","pricing","style","list-style:none")
-const list2 = dynamic("li","contact","style","list-style:none")
-const main = dynamic("main","","class","container")
-const section = dynamic("section","","class","container")
+// function dynamic(tagname,content,attributename,attributevalue){
+//     const ele = document.createElement(tagname);
+//     ele.innerHTML =content;
+//     ele.setAttribute(attributename,attributevalue)
+//     return ele;
+// }
+// const heading = dynamic("h1","create Element Task","class","head")
+// const heading1 = dynamic("h1","above muliple","class","head")
+// const header = dynamic("header","","class","header")
+// const navigation = dynamic("nav","","class","nav")
+// const order = dynamic("ul","","style","display:flex; justify-content:space-between; align-items:center; gap:20px; font-size:2rem")
+// const list = dynamic("li","about","style","list-style:none")
+// const list1 = dynamic("li","pricing","style","list-style:none")
+// const list2 = dynamic("li","contact","style","list-style:none")
+// const main = dynamic("main","","class","container")
+// const section = dynamic("section","","class","container")
 
-order.append(list,list1,list2)
-navigation.appendChild(order)
-header.appendChild(navigation)
-section.appendChild(heading1)
-main.appendChild(section)
-document.body.append(heading,header,main)
+// order.append(list,list1,list2)
+// navigation.appendChild(order)
+// header.appendChild(navigation)
+// section.appendChild(heading1)
+// main.appendChild(section)
+// document.body.append(heading,header,main)
+
+
+
+
+ 
